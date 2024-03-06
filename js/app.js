0b443a54176fb4a534832f366c8123486a8c10aa
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-06 13:46:53 +0100

    Added: FOR that print on console numbers form 1 to 100

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log("JS-FIZZBUZZ");
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+
+const n = 100;
+for(let i = 0; i < n; i++){
+    let num = i + 1;
+    console.log(num); //number
+}
+// per i multipli di 3 stampi “Fizz” al posto del numero
+// per i multipli di 5 stampi “Buzz” al post del numero
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero
